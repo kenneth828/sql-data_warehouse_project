64a82fe7803e70623b2a76b9bde6432d0c8da4e5
kenneth828 <79208022+kenneth828@users.noreply.github.com>
2025-12-15 19:55:49 +0700

    Create load_bronze.sql to load data into bronze layer
    This script loads data into the bronze layer of the Data Warehouse from various CSV files, truncating existing tables before loading new data.

---
+++ scripts/bronze/load_bronze.sql
@@ -0,0 +1,71 @@
+USE DataWarehouse;
+
+SELECT 'Loading Bronze Layer' AS msg;
+
+-- CRM CUSTOMER INFO
+SELECT 'Truncating bronze.crm_cust_info' AS msg;
+TRUNCATE TABLE bronze.crm_cust_info;
+
+LOAD DATA LOCAL INFILE '/Users/kenneth/sql-data-warehouse-project/datasets/source_crm/cust_info.csv'
+INTO TABLE bronze.crm_cust_info
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+-- CRM PRODUCT INFO
+SELECT 'Truncating bronze.crm_prd_info' AS msg;
+TRUNCATE TABLE bronze.crm_prd_info;
+
+LOAD DATA LOCAL INFILE '/Users/kenneth/sql-data-warehouse-project/datasets/source_crm/prd_info.csv'
+INTO TABLE bronze.crm_prd_info
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+-- CRM SALES DETAILS
+SELECT 'Truncating bronze.crm_sales_details' AS msg;
+TRUNCATE TABLE bronze.crm_sales_details;
+
+LOAD DATA LOCAL INFILE '/Users/kenneth/sql-data-warehouse-project/datasets/source_crm/sales_details.csv'
+INTO TABLE bronze.crm_sales_details
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+-- ERP LOCATION
+SELECT 'Truncating bronze.erp_loc_a101' AS msg;
+TRUNCATE TABLE bronze.erp_loc_a101;
+
+LOAD DATA LOCAL INFILE '/Users/kenneth/sql-data-warehouse-project/datasets/source_erp/LOC_A101.csv'
+INTO TABLE bronze.erp_loc_a101
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+-- ERP CUSTOMER
+SELECT 'Truncating bronze.erp_cust_az12' AS msg;
+TRUNCATE TABLE bronze.erp_cust_az12;
+
+LOAD DATA LOCAL INFILE '/Users/kenneth/sql-data-warehouse-project/datasets/source_erp/CUST_AZ12.csv'
+INTO TABLE bronze.erp_cust_az12
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+-- ERP PRICE CATEGORY
+SELECT 'Truncating bronze.erp_px_cat_g1v2' AS msg;
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+LOAD DATA LOCAL INFILE '/Users/kenneth/sql-data-warehouse-project/datasets/source_erp/PX_CAT_G1V2.csv'
+INTO TABLE bronze.erp_px_cat_g1v2
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+SELECT 'Bronze Layer Load Completed' AS msg;
